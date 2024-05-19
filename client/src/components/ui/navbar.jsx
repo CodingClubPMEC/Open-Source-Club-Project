f6102c8a32fabd34e1d5fd8e877bc0e92a07e58b
subhamc8 <subham020304@gmail.com>
2024-05-20 02:24:42 +0530

    added necessary links to navbar

--- client/src/components/ui/navbar.jsx
+++ client/src/components/ui/navbar.jsx
@@ -1,16 +1,19 @@
 import { Outlet, Link } from 'react-router-dom'
 
 export default function Navbar() {
-    return (
-        <>
-            <nav>
-                <h1> This is navbar</h1>
-                <ul>
-                    <li><Link to='/home'>Home</Link></li>
-                    <li><Link to='/auth'>Auth</Link></li>
-                </ul>
-                <Outlet />
-            </nav >
-        </>
-    )
+	return (
+		<>
+			<nav>
+				<ul>
+					<li><Link to='/home'>Home</Link></li>
+					<li><Link to='/trending'>Trending</Link></li>
+					<li><Link to='/search'>Search</Link></li>
+					<li><Link to='/club'>Club</Link></li>
+					<li><Link to='/profile'>Profile</Link></li>
+					<li><Link to='/feedback'>Feedback</Link></li>
+				</ul>
+				<Outlet />
+			</nav >
+		</>
+	)
 }
