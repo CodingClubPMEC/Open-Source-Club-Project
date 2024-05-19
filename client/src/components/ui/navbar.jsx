9ec8fda81295b957fbd3deb0a824500682c30c67
subhamc8 <subham020304@gmail.com>
2024-05-20 03:05:59 +0530

    styles added to navbar and ul is used as container for navbar

--- client/src/components/ui/navbar.jsx
+++ client/src/components/ui/navbar.jsx
@@ -1,10 +1,12 @@
 import { Outlet, Link } from 'react-router-dom'
 
 export default function Navbar() {
+	//TODO: Add icons instead of texts for links and make it look like buttons.
+	//TODO: Style the navbar to feel like ios dock.
 	return (
 		<>
-			<nav>
-				<ul>
+			<nav className='flex justify-center'>
+				<ul className='backdrop-blur-xl rounded-l w-11/12 flex flex-row justify-around items-center'>
 					<li><Link to='/home'>Home</Link></li>
 					<li><Link to='/trending'>Trending</Link></li>
 					<li><Link to='/search'>Search</Link></li>
